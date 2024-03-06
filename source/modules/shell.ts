0c62780082483846f22005ef428f1e7fcf5afb1b
angelomira <145370729+angelomira@users.noreply.github.com>
2024-03-07 02:10:08 +0300

    Update shell.ts

--- source/modules/shell.ts
+++ source/modules/shell.ts
@@ -1,16 +1,30 @@
 import shell from 'shelljs';
 
-function gitClone(repo_link: string, exec_path: string, clone_path: string = '.') {
+/**
+ * Clones a Git repository into the specified directory.
+ * @param {string} exec_path - The path to execute the Git clone command.
+ * @param {string} repo_link - The URL of the Git repository to clone.
+ * @param {string} [clone_path='.'] - The path to clone the repository into.
+ */
+function gitClone(exec_path, repo_link, clone_path = '.') {
     shell.cd(exec_path);
     shell.exec(`git clone ${repo_link} ${clone_path}`);
 }
 
+/**
+ * Pushes changes from the local repository to the remote repository.
+ * @param {string} exec_path - The path to execute the Git push command.
+ */
 function gitPush(exec_path) {
     shell.cd(exec_path);
     shell.exec('git push');
 }
 
+/**
+ * Pulls changes from the remote repository to the local repository.
+ * @param {string} exec_path - The path to execute the Git pull command.
+ */
 function gitPull(exec_path) {
     shell.cd(exec_path);
-    shell.exec('git pull')
+    shell.exec('git pull');
 }
