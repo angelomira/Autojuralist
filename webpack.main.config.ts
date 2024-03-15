c2a0f1d70446a474419b9ea6a1de7e9d9f989b3d
Axialer <capas2010@yandex.com>
2024-03-15 09:40:06 +0300

    -

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,4 +1,5 @@
 import type { Configuration } from 'webpack';
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
@@ -8,12 +9,16 @@ export const mainConfig: Configuration = {
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
+  target: "electron-main", // Указываем цель сборки для процесса main
   entry: './src/index.ts',
   // Put your normal webpack config below here
   module: {
     rules,
   },
-  plugins,
+  plugins: [
+    new NodePolyfillPlugin(), // Добавляем плагин для обеспечения совместимости с Node.js
+    ...plugins,
+  ],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
   },
